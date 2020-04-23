50e6b6a233b36e7450d6422ed3b84e0e6afae294
Joshua-Lafferty <61211256+Joshua-Lafferty@users.noreply.github.com>
2020-04-23 11:42:46 -0500

    Components added

---
+++ src/app/errorbox.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-errorbox',
+  templateUrl: './errorbox.component.html',
+  styleUrls: ['./errorbox.component.css']
+})
+export class ErrorboxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/noerrorpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-noerrorpage',
+  templateUrl: './noerrorpage.component.html',
+  styleUrls: ['./noerrorpage.component.css']
+})
+export class NoerrorpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
